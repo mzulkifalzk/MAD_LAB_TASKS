10a240e124f815851693d7459b21be3b23a3f1aa
Muhammad Zulkifal Qayyum <93385715+mzulkifalzk@users.noreply.github.com>
2022-04-12 01:29:32 +0500

    delete

--- CloneNetflix/App.js
+++
@@ -1,7 +0,0 @@
-import LandingPage from "./landingPage";
-import BackGroundImage from "./backGroundImage";
-
-export default function App() {
-  return <BackGroundImage></BackGroundImage>;
-  // <LandingPage></LandingPage>
-}
