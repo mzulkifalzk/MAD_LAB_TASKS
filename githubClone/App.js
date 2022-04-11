4fd92e9b0d2fc6de0c4bb2ebf3c6089fa86e0dbd
Muhammad Zulkifal Qayyum <93385715+mzulkifalzk@users.noreply.github.com>
2022-04-12 01:50:22 +0500

    Netflix Clone using React Native

---
+++ githubClone/App.js
@@ -0,0 +1,7 @@
+import LandingPage from "./landingPage";
+import BackGroundImage from "./backGroundImage";
+
+export default function App() {
+  return <BackGroundImage></BackGroundImage>;
+  // <LandingPage></LandingPage>
+}
